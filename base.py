d4203e3fdbaaa74fcb12930bc5c9cddda6607b53
alex <alex.delorenzo@gmail.com>
2015-09-25 18:21:27 -0400

    init

---
+++ base.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+
+# Match Terms
+#  A tuple of EXACT case-sensitive terms you know
+#  will be in your listing
+#
+#  IMPORTANT: visit the refurb section on apple.com
+#   and copy and paste the phrases you see there
+#   exactly as you see them
+FIND_TERMS = \
+    "8GB of", \
+    "256GB", \
+    "March 2015", \
+
+BASE_URL = "http://www.apple.com/"
+
+# Model Refurb URL
+#  Append the URL suffix to BASE_URL for the model of Mac you're looking for
+MODEL_REFURB_URL = \
+    BASE_URL + "shop/browse/home/specialdeals/mac/macbook_air/13"
+
+# Email information
+SEND_EMAIL = False
+RECEIVER_EMAIL = ""
+
+# Login information to send an email
+EMAIL_USERNAME = ""
+EMAIL = ""
+EMAIL_PASSWORD = ""
+
+# This assumes your SMTP server uses SSL
+SMTP_SERVER = ""
+SMTP_PORT = 587
+
+# Check apple.com every WAIT_SECONDS seconds
+WAIT_SECONDS = 60.0 * 3
+
+# Number of threads to keep in our email thread pool
+THREADS = 2
+
+LRU_CACHE_SIZE = 2
+
+
+class MacBook(namedtuple("MacBook", "title link price specs")):
+    def __str__(self):
+        return "<[%s] '%s' (%s)>" % (self.price, self.title, self.link)
