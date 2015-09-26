35883f9e566944434fed217240e622143947c9ac
alex <alex.delorenzo@gmail.com>
2015-09-25 21:02:12 -0400

    update-script

--- commit.sh
+++ commit.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 cp base.py old_base.py
 sed -i 's/\(EMAIL_USERNAME \=\) .*$/\1 ""/' base.py
 sed -i 's/\(EMAIL \=\) .*$/\1 ""/' base.py
@@ -6,7 +8,7 @@ sed -i 's/\(RECEIVER_EMAIL \=\) .*$/\1 ""/' base.py
 sed -i 's/\(SMTP_SERVER \=\) .*$/\1 ""/' base.py
 sed -i 's/\(SEND_EMAIL \=\) .*$/\1 False/' base.py
 cat base.py
-git add base.py
+git add base.py check_apple.py parse.py commit.sh requirements.txt send.py
 git commit -m $1
 mv old_base.py base.py
 
